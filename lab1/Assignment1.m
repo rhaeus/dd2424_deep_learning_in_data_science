feab484c941908e0b4b432126df8c47afbd9aaf8
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2021-04-01 19:13:01 +0200

    look at max diff in gradient computation

--- lab1/Assignment1.m
+++ lab1/Assignment1.m
@@ -19,31 +19,42 @@ X_test = X_test ./ repmat(std_X, [1, size(X_test,2)]);
 % init model
 [d, n] = size(X_train);
 K = 10;
+rng(400);
 W = 0.01*randn(K,d);
 b = 0.01*randn(K,1);
 
-P = EvaluateClassifier(X_train(1:20, 1), W(:, 1:20), b);
-
+% P = EvaluateClassifier(X_train(1:20, 1), W(:, 1:20), b);
 % J = ComputeCost(X_train, Y_train, W, b, 0.01)
-
 % acc = ComputeAccuracy(X_train, y_train, W, b)
-lambda = 0;
-disp('computing gradsnum')
-[ngrad_b, ngrad_W] = ComputeGradsNumSlow(X_train(1:20, 1), Y_train(:, 1), W(:, 1:20), b, lambda, 1e-6);
-disp('computing grads')
-[grad_W, grad_b] = ComputeGradients(X_train(1:20, 1), Y_train(:, 1), P, W(:, 1:20), lambda);
 
-diff_W = abs(ngrad_W - grad_W);
-diff_b = abs(ngrad_b - grad_b);
+lambda = 0;
+% P = EvaluateClassifier(X_train(1:20, 1), W(:, 1:20), b);
+% disp('computing gradsnum')
+% [ngrad_b, ngrad_W] = ComputeGradsNumSlow(X_train(1:20, 1), Y_train(:, 1), W(:, 1:20), b, lambda, 1e-6);
+% disp('computing grads')
+% [grad_W, grad_b] = ComputeGradients(X_train(1:20, 1), Y_train(:, 1), P, W(:, 1:20), lambda);
+
+P = EvaluateClassifier(X_train(:, 1), W, b);
+disp('computing gradsnum')
+[ngrad_b, ngrad_W] = ComputeGradsNumSlow(X_train(:, 1), Y_train(:, 1), W, b, lambda, 1e-6);
+disp('computing grads')
+[grad_W, grad_b] = ComputeGradients(X_train(:, 1), Y_train(:, 1), P, W, lambda);
+
+eps = 1e-6;
+diff_W = abs(ngrad_W - grad_W)./max(eps, abs(grad_W) + abs(ngrad_W));
+diff_b = abs(ngrad_b - grad_b)./max(eps, abs(grad_b) + abs(ngrad_b));
+
 
 if all(diff_W < 1e-6)
     disp('W ok')
 else 
-    disp('W not ok')
+    disp('W not ok, max diff: ')
+    max(max(diff_W))
 end
 
 if all(diff_b < 1e-6)
     disp('b ok')
 else 
-    disp('b not ok')
+    disp('b not ok, max diff: ')
+    max(max(diff_b))
 end
